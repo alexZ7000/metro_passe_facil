cd153d394d29ad586772ee21dceafbb17891982d
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-10-21 17:26:49 -0300

    :recycle: refactor: using react-native dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Routes from "./src/app/ui/routes/AppRoutes";
+
 import "react-native-gesture-handler";
+import { StatusBar } from "react-native";
 
 export default function App() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <NativeBaseProvider>
-                <StatusBar
-                    barStyle="dark-content"
-                    backgroundColor="transparent"
-                    translucent
-                />
-                <Routes />
-            </NativeBaseProvider>
+            <StatusBar
+                barStyle="dark-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Routes />
         </SafeAreaView>
     );
 }
