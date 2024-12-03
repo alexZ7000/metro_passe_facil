f4826e8319dac7228d49a49e0a78ae65f5d33b7d
Lucas Kenzo Tada <lkmtada@gmail.com>
2024-12-02 21:16:42 -0300

    versão com apk para buildar e merge na dev depois do reset

--- src/app/ui/screens/Signup.tsx
+++ src/app/ui/screens/Signup.tsx
@@ -23,8 +23,6 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Button, IconButton } from "react-native-paper";
 import { v4 as uuidv4 } from "uuid";
 
-// FIXME: Funcionalidade de abrir câmera não funciona para WEB
-
 export default function Signup() {
     const [nome, setNome] = useState("");
     const [dataNascimento, setDataNascimento] = useState("");
@@ -75,18 +73,38 @@ export default function Signup() {
     };
 
     const handleCadastro = async () => {
-        // Limpar os campos
-        setNome("");
-        setDataNascimento("");
-        setCpfRg("");
-        setTipoGratuidade("");
-        setImageUri(null);
+        if (!validateFields()) return;
 
-        // Exibir a mensagem de sucesso
-        Alert.alert("Sucesso", "Cadastro realizado com sucesso!");
+        try {
+            let imageUrl = null;
 
-        // Opcional: Navegar para a tela principal
-        navigation.navigate("MainTabs");
+            if (imageUri) {
+                const imageRef = ref(storage, `cadastros/${uuidv4()}`);
+                const response = await fetch(imageUri);
+                const blob = await response.blob();
+                await uploadBytes(imageRef, blob);
+                imageUrl = await getDownloadURL(imageRef); // Obtém a URL pública da imagem
+            }
+
+            const userId = uuidv4();
+
+            const cadastroData = {
+                id: userId,
+                nome,
+                dataNascimento,
+                cpfRg,
+                tipoGratuidade,
+                imageUrl
+            };
+
+            await addDoc(collection(db, "cadastros"), cadastroData);
+
+            Alert.alert("Sucesso", "Usuário cadastrado com sucesso!");
+            navigation.navigate("MainTabs");
+        } catch (error) {
+            console.error("Erro ao cadastrar usuário:", error);
+            Alert.alert("Erro", "Não foi possível concluir o cadastro.");
+        }
     };
 
     const handleImageUpload = async () => {
